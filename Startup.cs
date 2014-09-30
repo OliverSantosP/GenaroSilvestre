f7d45d79fdc57a2d9b948ebad715089366425e58
Oliver Santos Piña <oliversantosp@gmail.com>
2014-09-30 12:34:49 -0400

    Creating left aligned menu.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GenaroSilvestre.Startup))]
+namespace GenaroSilvestre
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
