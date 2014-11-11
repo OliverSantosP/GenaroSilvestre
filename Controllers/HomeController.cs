3e5346df3623c45c1905d84fdfaf8d3956206560
Oliver Santos Piña <oliversantosp@gmail.com>
2014-11-11 16:19:00 -0400

    Created Areas de Practica static page.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace GenaroSilvestre.Controllers
             return View();
         }
 
+        public ActionResult AreasDePractica()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
