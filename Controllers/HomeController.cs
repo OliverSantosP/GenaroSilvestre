024591015a0cc4052206855dc49a5900e2bbca5a
Oliver Santos Piña <oliversantosp@gmail.com>
2014-11-14 16:24:36 -0400

    Contact Form now sends emails.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GenaroSilvestre.Models;
+using System.Net.Mime;
+using System.Net.Mail;
 
 namespace GenaroSilvestre.Controllers
 {
@@ -29,8 +32,50 @@ namespace GenaroSilvestre.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Contactos.";
-
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(ContactModels Contacto)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                var Name = Contacto.Name;
+                var Address = Contacto.Address;
+                var Email = Contacto.Email;
+                var Phone = Contacto.Phone;
+                var City = Contacto.City;
+                var Comment = Contacto.Comment;
+                try
+                {
+                    MailMessage mailMsg = new MailMessage();
+                    mailMsg.To.Add(new MailAddress("oliversantosp@gmail.com", "Oliver Santos"));
+                    //mailMsg.To.Add(new MailAddress("srsamuelvasquez@gmail.com", "Samuel Vasquez"));
+                    mailMsg.From = new MailAddress("genarosilvestre@gmail.com", "GenaroSilvestre.com");
+
+                    mailMsg.Subject = "Nuevo mensaje desde GenaroSilvestre.com";
+                    string text = "Ha recibido un mensaje nuevo desde GenaroSilvestre.com. \n\n Nombre: "+Contacto.Name+ "\n Dirección: "+ Contacto.Address+ "\n Email: "+Contacto.Email+ "\n Telefono: "+Contacto.Phone+ "\n Ciudad: "+ Contacto.City+ "\n Comentario: "+Contacto.Comment;
+                    mailMsg.AlternateViews.Add(AlternateView.CreateAlternateViewFromString(text, null, MediaTypeNames.Text.Plain));
+
+                    SmtpClient smtpClient = new SmtpClient("smtp.sendgrid.net", Convert.ToInt32(587));
+                    System.Net.NetworkCredential credentials = new System.Net.NetworkCredential("oliversantosp", "Claro2014*");
+                    smtpClient.Credentials = credentials;
+                    smtpClient.Send(mailMsg);
+                }
+                catch (Exception ex)
+                {
+
+                }
+                ViewBag.ContactMessage = "Se ha enviado su mensaje.";
+                return View();
+            }
+            else
+            {
+                ViewBag.Message = "Contactos.";
+                return View();
+            }
+
         }
 
         public ActionResult Perfil()
---
+++ Models/ContactModels.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace GenaroSilvestre.Models
+{
+    public class ContactModels
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string Phone { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string Comment { get; set; }
+    }
+}
